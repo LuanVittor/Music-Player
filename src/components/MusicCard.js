21f05bcf5ea3c0d9b3311d8261df9e7bc45653a7
Luan Vitor <luan_vittor@hotmail.com>
2021-10-19 20:07:14 -0300

    Ex 07 done

---
+++ src/components/MusicCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends React.Component {
+  render() {
+    const { trackName, previewUrl } = this.props;
+    return (
+      <div>
+        <h3>{trackName}</h3>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          <code>audio</code>
+        </audio>
+      </div>
+    );
+  }
+}
+
+export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,12 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import Loading from './Loading';
+import MusicCard from '../components/MusicCard';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+    this.musicId = this.musicId.bind(this);
+    this.state = {
+      loading: true,
+      result: {
+        artistName: '',
+        collectionName: '',
+      },
+      track: [],
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    this.musicId(id);
+  }
+
+  async musicId(id) {
+    const results = await getMusics(id);
+    console.log(results);
+    console.log(results.slice(1));
+    const collection = results[0];
+    this.setState({ loading: false, result: collection, track: results.slice(1) });
+  }
+
   render() {
+    const { loading, result, track } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
         <p>Album</p>
+        {(loading) ? <Loading /> : (
+          <div>
+            <h2 data-testid="album-name">{ result.collectionName }</h2>
+            <h4 data-testid="artist-name">{ result.artistName }</h4>
+            {track.map(({ trackName, previewUrl }) => (
+              <MusicCard
+                trackName={ trackName }
+                previewUrl={ previewUrl }
+                key={ trackName }
+              />
+            ))}
+          </div>
+        )}
       </div>
     );
   }
