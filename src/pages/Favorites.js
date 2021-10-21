119a489aff299e6aa825531eeff9a247ea180177
Luan Vitor <luan_vittor@hotmail.com>
2021-10-21 17:34:31 -0300

    13?

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Favorites extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      favorites: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getFavorite();
+  }
+
+  async getFavorite() {
+    const result = await getFavoriteSongs();
+    // console.log(result);
+    this.setState({ favorites: result });
+  }
+
   render() {
+    const { favorites } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
-        <p>Favorites</p>
+        <h1>Favoritos</h1>
+        {(favorites) && favorites.map(({ trackName, previewUrl, trackId }) => (
+          <div key={ trackId }>
+            <MusicCard
+              favorites={ favorites }
+              trackName={ trackName }
+              previewUrl={ previewUrl }
+              key={ trackName }
+              trackId={ trackId }
+            />
+          </div>
+        ))}
       </div>
     );
   }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 import Header from '../components/Header';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.gettingUser = this.gettingUser.bind(this);
+    this.state = {
+      loading: false,
+      image: '',
+      name: '',
+      email: '',
+      description: '',
+    };
+  }
+
+  componentDidMount() {
+    this.gettingUser();
+  }
+
+  async gettingUser() {
+    this.setState({ loading: true });
+    const { name, email, image, description } = await getUser();
+    this.setState({ loading: false, name, email, image, description });
+  }
+
   render() {
+    const { loading, name, email, image, description } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        <p>Profile</p>
+        {(loading) ? <Loading /> : (
+          <div>
+            <h2>{ name }</h2>
+            <img data-testid="profile-image" src={ image } alt={ name } />
+            <h3>{ email }</h3>
+            <h3>{ description }</h3>
+            <Link to="/profile/edit"><p>Editar perfil</p></Link>
+          </div>
+        ) }
       </div>
     );
   }
 }
+
+// https://images.vexels.com/media/users/3/140753/isolated/lists/07326bd6157c14c5f8316e52b6c9dae4-avatar-de-perfil-masculino-7.png
 
 export default Profile;
